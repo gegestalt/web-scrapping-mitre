b2dd2e3b4869cba73b860b117fc2701881446e3c
gegestalt <guneyunal@sabanciuniv.edu>
2024-08-16 19:27:33 +0300

    tactic names can be fetched with the current script.

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 import puppeteer from "puppeteer";
+
 const url = "https://attack.mitre.org/matrices/enterprise/";
+
 const main = async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(url);
+
     const allTactics = await page.evaluate(() => {
-        const tactics = document.querySelectorAll('table');
-        return Array.from(tactics).map(tactic => {
-            const  tacticTitle = tactic.querySelector('tr');
-            const url = tactic.querySelector('a').href;
-            return{tacticTitle,url}   ;
-        
+        const cells = document.querySelectorAll('td.tactic.name a');
+        return Array.from(cells).map(cell => {
+            const tacticName = cell.textContent.trim();
+            const tacticUrl = cell.href;
+            return { tacticName, tacticUrl };
+        });
     });
-    });
+
     console.log(allTactics);
-}
-main()
+
+    await browser.close();
+};
+
+main();
